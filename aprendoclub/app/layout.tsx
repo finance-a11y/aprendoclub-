4841a21853b2e3bd439c381a4cdf9d63cd12698c
finance-a11y <finance@ariannalupi.com>
2026-04-09 12:46:15 -0400

    Update layout.tsx

--- aprendoclub/app/layout.tsx
+++ aprendoclub/app/layout.tsx
@@ -3,7 +3,30 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { MetaPixel } from "@/components/meta-pixel";
 import { GoogleAnalytics } from "@/components/google-analytics";
-
+export default function RootLayout({ children }) {
+  return (
+    <html lang="es">
+      <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{__html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-N75B48DX');
+        `}}/>
+      </head>
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript dangerouslySetInnerHTML={{__html: `
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N75B48DX"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        `}}/>
+        {children}
+      </body>
+    </html>
+  )
+}
 const montserrat = Montserrat({
   subsets: ["latin"],
   variable: "--font-montserrat",
